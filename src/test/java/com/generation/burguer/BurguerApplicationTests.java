9c33439a745053ec35b8bebcf8b61107a6bfa85f
Markinsky <carlosgerardo1957@hotmail.com>
2023-05-12 15:22:26 -0600

    creacion

---
+++ src/test/java/com/generation/burguer/BurguerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.burguer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurguerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
